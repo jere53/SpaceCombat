99c2894ec761f85b855effd66e66e4c6463e2bbb
Jeremias Salinas <64437798+jere53@users.noreply.github.com>
2023-07-27 21:37:48 -0300

    Finish implementing basic Projectiles
    Still need to figure out their visual effects and damage, but serviceable for now

--- Source/Weapons/Private/Projectile.cpp
+++ Source/Weapons/Private/Projectile.cpp
@@ -40,6 +40,18 @@ void AProjectile::BeginPlay()
 
 void AProjectile::OnOverlapBegin(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	//if we hit ourselves or another projectile we fired, do nothing
+	if (OtherActor == GetInstigator())
+	{
+		return;
+	}
+
+	if (OtherActor->GetInstigator() == GetInstigator())
+	{
+		return;
+	}
+
+
 	if (GEngine)
 		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Projectile Hit!!"));
 
